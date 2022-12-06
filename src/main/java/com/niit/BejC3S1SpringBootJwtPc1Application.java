633be38ac3569ae37ab94c480b9ab6f703d7f27e
Ashwani Rajput <ashwanir461@gmail.com>
2022-12-06 12:43:36 +0530

    done with the all package

---
+++ src/main/java/com/niit/BejC3S1SpringBootJwtPc1Application.java
@@ -0,0 +1,13 @@
+package com.niit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BejC3S1SpringBootJwtPc1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BejC3S1SpringBootJwtPc1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/niit/BejC3S1SpringBootJwtPc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BejC3S1SpringBootJwtPc1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
